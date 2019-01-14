69f3204e40fdce87901ac2e3d921611e3937e2ec
cervante <luca.panzavolta@gmail.com>
2019-01-14 15:42:59 +0100

    Another attempt for deployment.

---
+++ react-frontend/build/precache-manifest.2690ccc41770e935f5f22103a5f0dec8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6872279299fd2a0e8c881c9c3a872e27",
+    "url": "/static/media/010-mail.68722792.svg"
+  },
+  {
+    "revision": "f41ffe5ed73886646591",
+    "url": "/static/css/main.ccca24ce.chunk.css"
+  },
+  {
+    "revision": "382c8f819b19fb4b3007",
+    "url": "/static/js/1.382c8f81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dabcdaa294004a1e59704d4837497446",
+    "url": "/static/media/man.dabcdaa2.svg"
+  },
+  {
+    "revision": "77761e565c7a84bee918ea03f6126ba7",
+    "url": "/static/media/001-conference.77761e56.svg"
+  },
+  {
+    "revision": "f41ffe5ed73886646591",
+    "url": "/static/js/main.f41ffe5e.chunk.js"
+  },
+  {
+    "revision": "6b6feeaf4f746761d82baf782a77d3db",
+    "url": "/static/media/013-videocall-1.6b6feeaf.svg"
+  },
+  {
+    "revision": "2a0a42f8a14b7f655b7c1b1b9db831f6",
+    "url": "/static/media/024-megaphone.2a0a42f8.svg"
+  },
+  {
+    "revision": "1219b614b7395909187d198889c9c6c4",
+    "url": "/static/media/share-screen.1219b614.svg"
+  },
+  {
+    "revision": "781bd799bffc3c2213a1dd3c5bee6b4a",
+    "url": "/static/media/blackboard.781bd799.svg"
+  },
+  {
+    "revision": "973863ac921357835ad5379f493891dc",
+    "url": "/static/media/close-call.973863ac.svg"
+  },
+  {
+    "revision": "1f94963e3b7e343ccb207b5357ea16fd",
+    "url": "/index.html"
+  }
+];
