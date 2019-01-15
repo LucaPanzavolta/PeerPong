9c88793251a89b79d76ce5e47a0e0498fe77b4be
cervante <luca.panzavolta@gmail.com>
2019-01-15 19:41:43 +0100

    Commit pre-deployment

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ app.use(express.static(path.join(__dirname, '../react-frontend/build')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../react-frontend/build')));
 
-  /* app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = '../react-frontend/build/index.html'));
-  }) */
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname = '../react-frontend/build/index.html'));
+  })
 }
 
 const server = app.listen(PORT, function () {
